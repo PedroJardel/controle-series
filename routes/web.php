145bed146a289e16c79733338a9841537ab61e49
Pedro Lima <pedrozagolima@gmail.com>
2025-04-06 18:27:56 -0300

    feat: create function delete series

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/series');
 });
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)
+->only([ 'index', 'create', 'store', 'destroy']);
