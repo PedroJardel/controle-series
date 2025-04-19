ac06bf4e8e6c6baf62ad4f46ef3f633523fb3fad
Pedro Lima <pedrozagolima@gmail.com>
2025-04-19 14:27:07 -0300

    feat: create provider for connection SeriesRespository interface to EloquentSeriesRespository

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Repositories\SeriesRepository;
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
-use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+
+    }
     public function index()
     {
         $series = Series::all();
@@ -31,32 +34,8 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $series = DB::transaction(function () use ($request) {
-            $serie = Series::create($request->all());
-            $seasons = [];
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i,
-                ];
-            }
-            Season::insert($seasons);
-
-            $episodes = [];
-            foreach ($serie->seasons as $season) {
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j,
-                    ];
-                }
-            }
-            Episode::insert($episodes);
-
-            return $serie;
-        });
-
-        return to_route('series.index')->with('message.success', "Série '{$series->name}' adicionada com sucesso!");
+        $serie = $this->seriesRepository->add($request);
+        return to_route('series.index')->with('message.success', "Série '{$serie->name}' adicionada com sucesso!");
     }
 
     public function edit(Series $series)
---
+++ app/Providers/SeriesRepositoryProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Repositories\EloquentSeriesRepository;
+use App\Http\Repositories\SeriesRepository;
+use Illuminate\Support\ServiceProvider;
+
+class SeriesRepositoryProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+}
