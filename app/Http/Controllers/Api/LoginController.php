809e2e13bb8e56a30eb2fe181a5937927fa15f45
Pedro Lima <pedrozagolima@gmail.com>
2025-04-27 19:46:19 -0300

    feat: implements sanctum authentication

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authentication(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        if(Auth::attempt($credentials) === false) {
+            return response()->json(['message' => 'Invalid credentials.'], 401);
+        };
+        
+        $user = Auth::user();
+        $token = $user->createToken(
+            'token',
+            ['*'],
+            now()->addDays(1)
+
+        );
+        return response()->json(['token' => $token->plainTextToken]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EpisodesController;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\SeasonsController;
 use App\Http\Controllers\Api\SeriesController;
 use Illuminate\Http\Request;
@@ -10,7 +11,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index']);
-Route::get('/series/{series}/episodes', [EpisodesController::class, 'index']);
-Route::patch('/episodes/{episode}', [EpisodesController::class, 'watched']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index']);
+    Route::get('/series/{series}/episodes', [EpisodesController::class, 'index']);
+    Route::patch('/episodes/{episode}', [EpisodesController::class, 'watched']);
+});
+Route::post('/login', [LoginController::class, 'authentication']);
