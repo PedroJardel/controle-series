a7dc6b05c26438adfdf4e8e59b8aa3575600c5e2
Pedro Lima <pedrozagolima@gmail.com>
2025-04-27 17:15:43 -0300

    Getting to know accessors and mutators

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Episode extends Model
@@ -15,4 +16,15 @@ class Episode extends Model
     {
         return $this->belongsTo(Season::class);
     }
+
+    /** 
+    // Acessors e Mutators
+    protected function watched(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($watched) => (bool) $watched,
+            set: fn ($watched) => (bool) $watched,
+        );
+    }
+    */
 }
